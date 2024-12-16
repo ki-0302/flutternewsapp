a5e3785886c766d5915ac0b891fb2b563d6be336
ki-0302 <support@maho-ya.com>
2024-12-17 08:33:04 +0900

    feat: modify text style

--- lib/constants_text_style.dart
+++ lib/constants_text_style.dart
@@ -3,18 +3,42 @@ import 'package:flutter/material.dart';
 class ConstantsTextStyle {
   ConstantsTextStyle._();
 
-  static TextStyle source =  const TextStyle(
+  static TextStyle listSource =  const TextStyle(
     color: Colors.black,
     fontSize: 12,
+  );
+
+  static TextStyle listTitle =  const TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle listPublishedAt =  TextStyle(
+    color: Colors.grey[800],
+    fontSize: 12,
+  );
+
+  static TextStyle source =  const TextStyle(
+    color: Colors.black,
+    fontSize: 14,
   );
 
   static TextStyle title =  const TextStyle(
     color: Colors.black,
-    fontSize: 16,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
   );
 
   static TextStyle publishedAt =  TextStyle(
     color: Colors.grey[800],
-    fontSize: 12,
+    fontSize: 14,
   );
+  
+  static TextStyle body =  const TextStyle(
+    color: Colors.black,
+    fontSize: 17,
+  );
+
+
 }
